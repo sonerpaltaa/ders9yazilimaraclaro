9391b7308a7675537e26bd14ce93dd394b6a7d29
bprlab <bprlab@10.200.144.12>
2019-04-27 13:04:56 +0300

    Mesaj

---
+++ ders09ornek/src/main/java/tr/medipol/edu/tr/ders09ornek/Ders09ornekApplication.java
@@ -0,0 +1,13 @@
+package tr.medipol.edu.tr.ders09ornek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ders09ornekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ders09ornekApplication.class, args);
+	}
+
+}
---
+++ ders09ornek/src/test/java/tr/medipol/edu/tr/ders09ornek/Ders09ornekApplicationTests.java
@@ -0,0 +1,16 @@
+package tr.medipol.edu.tr.ders09ornek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ders09ornekApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
